a371d8f230dca87a7b2eff75c97201cf3030d945
Phong Cao <phongcao0714@gmail.com>
2023-01-16 11:11:42 -0800

    Implement function to serve mxl files

--- index.js
+++ index.js
@@ -2,10 +2,26 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const fs = require('fs');
 
 app.use(cors())
+// app.use(express.static('musicgenerator/Baroque'))
+// app.use(express.static('musicgenerator/Classical'))
+// app.use(express.static('musicgenerator/Romantic'))
 app.use(express.static('build'))
 app.use(express.json())
+
+
+app.get('/romantic', async (req, res) => {
+    const fileDirectory = './musicgenerator/Romantic/'
+    fs.readdir(fileDirectory, (err, files) => {
+        randomFile = files[Math.floor(Math.random()*files.length)];
+        res.sendFile(randomFile, {root: fileDirectory}, (err) => {
+            res.end();
+            if (err) throw(err);
+        })
+    })
+})
 
 
 
